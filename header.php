5e7da1f14689a8a7447b0dbef9239f827fdaa41f
norielagartosacristan <norielagartosacristan@gmail.com>
2024-11-10 17:36:13 +0800

    add

--- header.php
+++ header.php
@@ -11,24 +11,26 @@
 <body>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Greyhawk Travel and Tours</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="tour_packages.php">Tour Packages</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="flight_hotels.php">Flight & Hotel Booking</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact_us.php">Contact Us</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <a class="navbar-brand" href="#">
+        <img src="images/logo.png" alt="Greyhawk Travel and Tours Logo" style="height: 40px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="tour_packages.php">Holiday</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="flight_hotels.php">Flight & Hotel Booking</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact_us.php">Contact Us</a>
+            </li>
+        </ul>
+    </div>
+</nav>
