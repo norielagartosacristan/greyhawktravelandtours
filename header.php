fd05cc0a3f1f35d8b02bf7bcf6b19938d230072b
norielagartosacristan <norielagartosacristan@gmail.com>
2024-11-19 12:20:28 +0800

    add

--- header.php
+++ header.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M4JPNFC');</script>
+<!-- End Google Tag Manager -->
+
     <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-DY5928N5XH"></script>
 <script>
@@ -28,6 +37,12 @@
     </style>
 </head>
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4JPNFC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+ 
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#">
--- index.php
+++ index.php
@@ -1,42 +1,18 @@
 <?php include "header.php"; ?>
 
-<?php
-require 'includes/dbh.inc.php';
-
-// Fetch a few featured tour packages to display on the home page
-$query = "SELECT * FROM Tour_Packages LIMIT 3"; // You can change the limit based on how many you want to show
-$result = mysqli_query($conn, $query);
-?>
 
     <!-- Banner Section -->
     <div class="jumbotron jumbotron-fluid text-white custom-bg">
         <div class="container">
-            <h1 class="display-4">Explore the World with Greyhawk</h1>
-            <p class="lead">Discover exciting destinations and personalized tour packages just for you!</p>
+            <h1 class="display-4">Discover the world with adventurous souls</h1>
+            <div class="text-container">
+                <p class="lead justify-text">At Greyhawk Travel and Tours, every journey is more than a destination-it's a shared adventure.
+                Together with like-minded travelers, explore new horizons, uncover hidden gems, and embrace the thrill of the unknown.
+                Whether you're seeking vibrant cultures or tranquil escapes, we bring people together to experience the world in unforgetable ways</p>
+            </div>
             <a href="tour_packages.php" class="btn btn-light">View All Tour Packages</a>
         </div>
     </div>
-
-    <!-- Featured Tour Packages 
-    <div class="container mt-5">
-        <h2 class="text-center">Featured Tour Packages</h2>
-        <div class="row">
-            <?php while($row = mysqli_fetch_assoc($result)): ?>
-                <div class="col-md-4">
-                    <div class="card mb-4">
-                        <img src="<?php echo $row['image_url']; ?>" class="card-img-top" alt="<?php echo $row['title']; ?>">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $row['title']; ?></h5>
-                            <p class="card-text"><?php echo substr($row['description'], 0, 100); ?>...</p>
-                            <p class="card-text"><strong>Price:</strong> PHP <?php echo $row['price']; ?></p>
-                            <a href="tour_packages.php" class="btn btn-primary">View Details</a>
-                        </div>
-                    </div>
-                </div>
-            <?php endwhile; ?>
-        </div>
-    </div>
-            -->
 
 <div class="city-grid">
     <div class="city-image">
